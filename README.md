bfdfaf0a8a6bef00adac9e63a8c98d7c775a2ddc
Lakpa Dietz <ldietz086@gmail.com>
2023-01-16 10:57:41 -0800

    Added README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Luna
+
+**_Luna is catered towards lovers of astrophotography. The website was built using HTML, CSS and JavaScript and the data is provided by the NASA Astronomy Picture of the Day API._**
+
+![NASA Website gif](./media/nasa.gif)
--- js/main.js
+++ js/main.js
@@ -9,13 +9,15 @@ function getFetch() {
     .then((data) => {
       if (data.media_type === "image") {
         document.querySelector("img").src = data.hdurl;
+        document.querySelector("h2").innerText = data.title;
+        document.querySelector("h3").innerText = data.explanation;
         document.querySelector("iframe").style.display = "none";
       } else if (data.media_type === "video") {
         document.querySelector("iframe").src = data.url;
+        document.querySelector("h2").innerText = data.title;
+        document.querySelector("h3").innerText = data.explanation;
         document.querySelector("img").style.display = "none";
       }
-      document.querySelector("h2").innerText = data.title;
-      document.querySelector("h3").innerText = data.explanation;
     })
     .catch((err) => {
       console.log(`error ${err}`);
